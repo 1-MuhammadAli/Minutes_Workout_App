40134011a496f2d22de9395aeb2788770843c8b1
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-17 15:21:33 +0500

    Finish Activity : Added -> the finish activity

---
+++ app/src/main/java/com/example/minutesworkout/FinishActivity.kt
@@ -0,0 +1,33 @@
+package com.example.minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.minutesworkout.databinding.ActivityFinishBinding
+
+class FinishActivity : AppCompatActivity() {
+    //Todo 1: Create a binding variable
+    private var binding: ActivityFinishBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//Todo 2: inflate the layout
+        binding = ActivityFinishBinding.inflate(layoutInflater)
+//Todo 3: bind the layout to this Activity
+        setContentView(binding?.root)
+//Todo 4: attach the layout to this activity
+        setSupportActionBar(binding?.toolbarFinishActivity)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarFinishActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        //END
+
+        //TODO(Step 6 : Adding a click event to the Finish Button.)
+        //START
+        binding?.btnFinish?.setOnClickListener {
+            finish()
+        }
+    }
+}
