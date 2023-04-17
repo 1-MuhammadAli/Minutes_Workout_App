11e7523176a0ff65be7a3317a5713b02e6cd9cea
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-17 13:35:33 +0500

    Exercise Activity : Added -> Displaying at witch exercise we currently are

--- app/src/main/java/com/example/minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/minutesworkout/ExerciseActivity.kt
@@ -133,6 +133,9 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
             override fun onFinish() {
                 currentExercisePosition++
+
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
                 setupExerciseView()
             }
         }.start()
@@ -149,6 +152,11 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
 
             override fun onFinish() {
+
+                exerciseList!![currentExercisePosition].setIsSelected(false)
+                exerciseList!![currentExercisePosition].setIsCompleted(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
                 if (currentExercisePosition<exerciseList?.size!! - 1){
                     setupRestView()
                 }else{
